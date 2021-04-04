5f1da0cff55ae08f1301dc28cc46371264d202cd
Emin Martinian <emin.martinian@gmail.com>
2021-04-04 19:44:13 -0400

    More robust exection of wrapped command

--- ox_ui/core/c2f.py
+++ ox_ui/core/c2f.py
@@ -1,6 +1,7 @@
 """Tools to convert click commands to flask WTForms
 """
 
+import datetime
 import tempfile
 import re
 import logging
@@ -15,7 +16,8 @@ from jinja2 import Environment, BaseLoader
 from click import types
 import click_datetime
 
-from wtforms import BooleanField, StringField, IntegerField, DateTimeField
+from wtforms import widgets
+from wtforms import BooleanField, StringField, IntegerField, Field
 
 from ox_ui import core as ox_ui_core
 from ox_ui.assets import css
@@ -42,7 +44,8 @@ class FileResponseTweak:
         return self.__class__.__name__
 
     def post_process_result(self, cmd, result):
-        logging.debug('Tweak %s ignores previous result of %s', self.name())
+        logging.debug('Tweak %s ignores previous result of %s', self.name(),
+                      result)
         response = None
         with open(self.file_name) as my_fd:
             suffix = self.mktemp_kwargs.get('suffix', None)
@@ -59,6 +62,33 @@ class FileResponseTweak:
             logging.info('Removing temporary file %s', self.file_name)
             os.remove(self.file_name)
         return response
+
+
+class DateTimeFieldTweak(Field):
+    """Ttext field which stores a `datetime.datetime` from multiple formats.
+    """
+    widget = widgets.TextInput()
+
+    def __init__(self, label=None, validators=None,
+                 formats=('%Y-%m-%d %H:%M:%S', '%Y-%m-%d'), **kwargs):
+        super(DateTimeFieldTweak, self).__init__(label, validators, **kwargs)
+        self.formats = formats
+
+    def _value(self):
+        if self.raw_data:
+            return ' '.join(self.raw_data)
+        return self.data and self.data.strftime(self.formats[0]) or ''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            date_str = ' '.join(valuelist)
+            for fmt in self.formats:
+                try:
+                    self.data = datetime.datetime.strptime(date_str, fmt)
+                    return
+                except ValueError:
+                    self.data = None
+        raise ValueError(self.gettext('Not a valid datetime value'))
 
 
 class ClickToWTF:
@@ -103,7 +133,7 @@ class ClickToWTF:
     def form(self):
         cls = self.form_cls()
         return cls()
-            
+
     def click_opt_to_wtf_field(self, opt):
         if opt.type == types.INT:
             field = IntegerField(opt.name, validators=[], description=str(
@@ -111,9 +141,21 @@ class ClickToWTF:
         elif opt.type == types.STRING:
             field = StringField(opt.name, validators=[], description=str(
                 opt.help), default=opt.default)
-        elif isinstance(opt.type, click_datetime.Datetime):
-            field = DateTimeField(opt.name, validators=[], description=str(
-                opt.help), default=opt.default)
+        elif isinstance(opt.type, click_datetime.Datetime) or (
+                getattr(opt.type, 'name', '?') == 'datetime'):
+            kwargs = {}
+            if hasattr(opt, 'foramts'):
+                kwargs['formats'] = opt.formats
+            default = opt.default
+            if callable(default):
+                default = default()
+            if isinstance(default, str):
+                default = datetime.datetime.strptime(
+                    default, '%Y-%m-%d %H:%M:%S')
+
+            field = DateTimeFieldTweak(
+                opt.name, validators=[], description=str(opt.help),
+                default=default, **kwargs)
         else:
             raise TypeError('Cannot represent click type %s in WTF' % (
                 opt.type))
@@ -124,11 +166,15 @@ class ClickToWTF:
                   for opt in self.clickCmd.params
                   if opt.name not in self.gobbled_opts}
         self.pad_kwargs(kwargs)
-        raw_result = self.clickCmd.callback(**kwargs)
+        wrapped_cmd = getattr(self.clickCmd.callback, '__wrapped__', None)
+        if not wrapped_cmd:
+            raise ValueError('Could not find wrapped function for %s.%s' % (
+                str(self.clickCmd), str(self.clickCmd.callback)))
+        raw_result = wrapped_cmd(**kwargs)
         result = raw_result
         for tweak in self.tweaks:
             result = tweak.post_process_result(self, result)
-        
+
         return result
 
     def post_process_result(self, raw_result):
@@ -163,4 +209,3 @@ class ClickToWTF:
             return result
         logging.debug('Showing form either because invalid or first view')
         return self.show_form(form=form)
-
